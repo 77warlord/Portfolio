7c33014d8015ad1da44744fc007de5dd4b1cb176
Aryan-8d <72217163+77warlord@users.noreply.github.com>
2023-10-20 01:16:10 +0530

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,13 +9,13 @@ const Work = () => {
     //setProject(data);
   
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#000000]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-600'>
             Work
           </p>
-          <p className='py-6'>// Check out some of my recent work</p>
+          <p className='py-6 md:font-bold'>Check out some of my recent work...</p>
         </div>
 
 {/* container for projects */}
@@ -26,12 +26,12 @@ const Work = () => {
   <div
     key={index}
     style={{ backgroundImage: `url(${item.image})` }}
-    className="shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div "
+    className="shadow-lg shadow-[#e9ed7b] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div hover:scale-110 duration-500"
   >
     {/* Hover effect for images */}
-    <div className="opacity-0 group-hover:opacity-100 ">
-      <span className="text-2xl font bold text-white tracking-wider ">
+    <div className="opacity-0 group-hover:opacity-100">
+      <span className="text-2xl font-bold text-white tracking-wider ">
         {item.name}
       </span>
       <div className="pt-8 text-center ">
@@ -39,7 +39,7 @@ const Work = () => {
         <a href={item.github} target="_blank">
           <button
             className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
+                       bg-black text-white font-bold text-lg"
           >
             Code
           </button>
@@ -48,7 +48,7 @@ const Work = () => {
         <a href={item.live} target="_blank">
           <button
             className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
+            bg-black text-white  font-bold text-lg"
           >
             Live
           </button>
